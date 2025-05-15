43846cc0cc663fc02c4d894372c695a0ee706a3a
astapovad <astapovad@gmail.com>
2025-05-15 14:16:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ prompt_styles = {
 user_input = st.text_area("Введіть свій текст:")
 
 if st.button("Згенерувати текст"):
-     st.session_state["generated"] = True
+     st.session_state["Згенерувати текст"] = True
     if user_input:
         full_prompt = f"{prompt_styles[style]} {user_input}"
         with st.spinner("Генерується..."):
